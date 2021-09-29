48f8f212725dd4354c2c6b934ec6fbad21be9da1
Tchilo <tchiloross@gmail.com>
2021-09-29 12:21:08 +0200

    Update README.md file

--- test/test.test.js
+++ test/test.test.js
@@ -4,27 +4,36 @@ const sub = require('../js/subtract');
 const div = require('../js/divid');
 const howLong = require('../js/string');
 const caps = require('../js/capital');
+const reversString = require('../js/reverseString');
 
-test('returns sum of a, b', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('calculate', () => {
+  test('returns sum of a, b', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test('return multiplied num of a ,b', () => {
+    expect(multi(5, 10)).toBe(50);
+  });
+
+  test('return subtracted num of a ,b', () => {
+    expect(sub(10, 5)).toBe(5);
+  });
+
+  test('return subtracted num of a ,b', () => {
+    expect(div(10, 5)).toBe(2);
+  });
 });
 
-test('return multiplied num of a ,b', () => {
-  expect(multi(5, 10)).toBe(50);
-});
+describe('String checks', () => {
+  test('return length of string', () => {
+    expect(howLong('Taro')).toEqual(4);
+  });
 
-test('return subtracted num of a ,b', () => {
-  expect(sub(10, 5)).toBe(5);
-});
+  test('first letter is upperCased', () => {
+    expect(caps('taro')).toBe('Taro');
+  });
 
-test('return subtracted num of a ,b', () => {
-  expect(div(10, 5)).toBe(2);
+  test('Should reverse a string', () => {
+    expect(reversString('Taro')).toBe('oraT');
+  });
 });
-
-test('return length of string', () => {
-  expect(howLong('Taro')).tobe(4);
-});
-
-test('first letter is upperCased', () => {
-  expect(caps('taro')).toBe('Taro');
-})
